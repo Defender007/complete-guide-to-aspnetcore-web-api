5959e27089c2470dc6f58f581f2dda34c8eba764
Defender007 <mydjangopythonprojects@gmail.com>
2021-05-12 17:53:01 +0100

    added EF and seeded some data...

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+using System;
+using Microsoft.EntityFrameworkCore;
+using my_books.Data.Models;
+
+namespace my_books.Data
+{
+    public class AppDBContext: DbContext
+    {
+        public AppDBContext(DbContextOptions<AppDBContext> options):base(options)
+        {
+            
+        }
+        public DbSet<Book> Books {get; set;}
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -6,34 +6,36 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using my_books.Data;
 
 namespace my_books
 {
     public class Startup
     {
+        public string ConnectionString { get; set; }
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
+            ConnectionString = Configuration.GetConnectionString("DefaultConnectionString");
         }
-
         public IConfiguration Configuration { get; }
-
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllers();
+            // Configure DBContext with SQL
+            services.AddDbContext<AppDBContext>(options=>options.UseSqlServer(ConnectionString));
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "my_books", Version = "v1" });
+                c.SwaggerDoc("v2", new OpenApiInfo { Title = "my_books_updated_tittle", Version = "v2" });
             });
         }
-
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
@@ -41,7 +43,7 @@ namespace my_books
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "my_books v1"));
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v2/swagger.json", "my_books_ui_updated v2"));
             }
 
             app.UseHttpsRedirection();
@@ -54,6 +56,7 @@ namespace my_books
             {
                 endpoints.MapControllers();
             });
+            AppDbInitializer.Seed(app);
         }
     }
 }
